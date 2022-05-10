7ce63b7ecb4d387b30d713ffad870748757d716c
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2022-05-10 11:32:35 +0300

    added hotels routes

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -13,9 +13,51 @@ router.post("/", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
 //update
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedHotel);
+  } catch (error) {
+    res.status(500).json(err);
+  }
+});
+
 //delete
+router.delete("/:id", async (req, res) => {
+  try {
+    const updatedHotel = await Hotel.findByIdAndDelete(req.params.id);
+    res.status(200).json("Hotel has been deleted");
+  } catch (error) {
+    res.status(500).json(err);
+  }
+});
+
 //get
+router.get("/:id", async (req, res) => {
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    res.status(200).json(hotel);
+  } catch (error) {
+    res.status(500).json(err);
+  }
+});
+
 //get all
+router.get("/", async (req, res) => {
+    try {
+      const hotels = await Hotel.find();
+      res.status(200).json(hotels);
+    } catch (error) {
+      res.status(500).json(err);
+    }
+  });
 
 export default router;
