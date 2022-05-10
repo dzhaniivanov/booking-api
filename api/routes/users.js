a618e1f0585a513633151251b8e518578a71c524
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2022-05-10 16:39:33 +0300

    jwt token sign and verify added

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,10 +1,28 @@
 import express from "express";
+import {
+  deleteUser,
+  getUser,
+  getUsers,
+  updateUser,
+} from "../controllers/user.js";
+import { verrifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("users endpoint");
+router.get("/checkauthentication", verrifyToken, (req, res, next) => {
+  res.send("hello user,you are logged in");
 });
 
+//update
+router.put("/:id", updateUser);
+
+//delete
+router.delete("/:id", deleteUser);
+
+//get
+router.get("/:id", getUser);
+
+//get all
+router.get("/", getUsers);
 
 export default router;
