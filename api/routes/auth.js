65e03c2fb291781eec9f3164f5e8541530428565
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2022-05-10 10:41:47 +0300

    setup routes

---
+++ api/routes/auth.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("auth endpoint");
+});
+router.get("/register", (req, res) => {
+  res.send("register endpoint");
+});
+
+export default router;
