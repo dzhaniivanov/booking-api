a06a90c6d516845edbc805e018f7de3399b1c879
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2022-05-11 09:52:30 +0300

    added room model,controllers,routes

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,10 +1,28 @@
 import express from "express";
+import {
+  createRoom,
+  updateRoom,
+  deleteRoom,
+  getRoom,
+  getRooms,
+} from "../controllers/room.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("rooms endpoint");
-});
+//create
+router.post("/:hotelid", verifyAdmin, createRoom);
 
+//update
+router.put("/:id", verifyAdmin, updateRoom);
+
+//delete
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
+
+//get
+router.get("/:id", getRoom);
+
+//get all
+router.get("/", getRooms);
 
 export default router;
