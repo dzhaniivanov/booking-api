186826d12a5ac52c07a8882d235be9170fed86e5
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2022-05-11 15:44:00 +0300

    bug with git fixed

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import List from "./pages/List/List";
+import Home from "./pages/Home/Home";
+import Hotel from "./pages/Hotel/Hotel";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hotels" element={<List />} />
+        <Route path="/hotels/:id" element={<Hotel />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ client/src/components/Featured/Featured.js
@@ -0,0 +1,54 @@
+import useFetch from "../../hooks/useFetch";
+import "./Featured.css";
+
+const Featured = () => {
+  const { data, loading, error } = useFetch(
+    "/hotels/countByCity?cities=berlin,madrid,london"
+  );
+
+  return (
+    <div className="featured">
+      {loading ? (
+        "Loading please wait..."
+      ) : (
+        <>
+          <div className="featuredItem">
+            <img
+              src="https://cf.bstatic.com/xdata/images/city/max500/674375.webp?k=1e119b344cfb80595c538029ec77302bdafdb755d90de3cc899e56233eba289e&o="
+              alt=""
+              className="featuredImg"
+            />
+            <div className="featuredTitles">
+              <h1>Berlin</h1>
+              <h2>{data[0]} properties</h2>
+            </div>
+          </div>
+          <div className="featuredItem">
+            <img
+              src="https://cf.bstatic.com/xdata/images/city/max500/619896.webp?k=9364e91ae738a47a4a37b0a7fb26d01e5e95db810f7167e9e5ecf58e5ca74e9b&o="
+              alt=""
+              className="featuredImg"
+            />
+            <div className="featuredTitles">
+              <h1>Madrid</h1>
+              <h2>{data[1]} properties</h2>
+            </div>
+          </div>
+          <div className="featuredItem">
+            <img
+              src="https://cf.bstatic.com/xdata/images/city/max500/656081.webp?k=31f1f24789ea74eca368f90ecad47868bd09514d66c72fd96798c5ab0cf7625b&o="
+              alt=""
+              className="featuredImg"
+            />
+            <div className="featuredTitles">
+              <h1>London</h1>
+              <h2>{data[2]} properties</h2>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Featured;
