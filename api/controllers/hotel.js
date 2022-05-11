483ee90d6f567460a8c05ed0e53b44b36df73d75
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2022-05-11 11:06:41 +0300

    fetch featured and property list from db

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -53,9 +53,11 @@ export const getHotels = async (req, res, next) => {
 export const countByCity = async (req, res, next) => {
   const cities = req.query.cities.split(",");
   try {
-    const list=await Promise.all(cities.map(city=>{
-      return Hotel.countDocuments({city:city})
-    }))
+    const list = await Promise.all(
+      cities.map((city) => {
+        return Hotel.countDocuments({ city: city });
+      })
+    );
     res.status(200).json(list);
   } catch (error) {
     next(error);
@@ -64,8 +66,19 @@ export const countByCity = async (req, res, next) => {
 
 export const countByType = async (req, res, next) => {
   try {
-    const hotels = await Hotel.find();
-    res.status(200).json(hotels);
+    const hotelCount = await Hotel.countDocuments({ type: "hotel" });
+    const apartmentCount = await Hotel.countDocuments({ type: "apartment" });
+    const resortCount = await Hotel.countDocuments({ type: "resort" });
+    const villaCount = await Hotel.countDocuments({ type: "villa" });
+    const cabinCount = await Hotel.countDocuments({ type: "cabin" });
+
+    res.status(200).json([
+      { type: "hotel", count: hotelCount },
+      { type: "apartments", count: apartmentCount },
+      { type: "resort", count: resortCount },
+      { type: "villas", count: villaCount },
+      { type: "cabins", count: cabinCount },
+    ]);
   } catch (error) {
     next(error);
   }
